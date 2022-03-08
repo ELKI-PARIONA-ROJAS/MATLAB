533bfe95bb98b1b17b7b66383c9041f548d03ba7
ELKE-PARIONA-ROJAS <elkiparionarojas@gmail.com>
2022-03-07 23:51:02 -0500

    final

---
+++ programacion1.m
@@ -0,0 +1,8 @@
+	numeros=36; % numero maximo dento de la matriz
+    
+    % inicializacion de variables
+    % ---------------------------
+    filas=0; 
+    columnas=0; 
+    matriz=0; 
+    % ---------------------------

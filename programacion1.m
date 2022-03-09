c8c1059378c6d71b318ecc5a473effbff65ad516
ELKI PARIONA ROJAS <73377598+ELKI-PARIONA-ROJAS@users.noreply.github.com>
2022-03-08 22:41:48 -0500

    Delete programacion1.m
    Porque este archivo no contiene los codigos necesarios

--- programacion1.m
+++
@@ -1,8 +0,0 @@
-	numeros=36; % numero maximo dento de la matriz
-    
-    % inicializacion de variables
-    % ---------------------------
-    filas=0; 
-    columnas=0; 
-    matriz=0; 
-    % ---------------------------
